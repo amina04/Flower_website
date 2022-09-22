7864362e8ce3838f83f94ead1a2e281e0f81f1a4
amina04 <khachaamina0@gmail.com>
2022-09-23 01:03:55 +0200

    Updates

---
+++ precache-manifest.6fb5b8bb14ffd888071efc1038afd64a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca017aef10efe5e08d5e",
+    "url": "/flower_website/static/js/main.ca017aef.chunk.js"
+  },
+  {
+    "revision": "1ac88f23a1eff080feff",
+    "url": "/flower_website/static/js/1.1ac88f23.chunk.js"
+  },
+  {
+    "revision": "5ecc5b62d3ed18938406",
+    "url": "/flower_website/static/js/runtime~main.5ecc5b62.js"
+  },
+  {
+    "revision": "1d5ca9b219c775cc0afdc9523f37f77d",
+    "url": "/flower_website/static/media/second.1d5ca9b2.png"
+  },
+  {
+    "revision": "5c68ac0228a8f9284b20be19dbb63198",
+    "url": "/flower_website/static/media/first.5c68ac02.png"
+  },
+  {
+    "revision": "7e1c4ee63c83300955319aa5713c60b2",
+    "url": "/flower_website/static/media/2.7e1c4ee6.png"
+  },
+  {
+    "revision": "c98b4fefca376f0637a97046a4d6793a",
+    "url": "/flower_website/static/media/5.c98b4fef.jpg"
+  },
+  {
+    "revision": "e19eea1fda424b9ba61434e0b60f884f",
+    "url": "/flower_website/static/media/6.e19eea1f.jpg"
+  },
+  {
+    "revision": "0ef50394bc3f21678ed0b48877d5f6a5",
+    "url": "/flower_website/static/media/1.0ef50394.png"
+  },
+  {
+    "revision": "bd3681c8e2f4b5f0c92fc3ec0a474809",
+    "url": "/flower_website/static/media/3.bd3681c8.png"
+  },
+  {
+    "revision": "f005cafe36ad400cc947735b6e4942b4",
+    "url": "/flower_website/static/media/4.f005cafe.png"
+  },
+  {
+    "revision": "28316b0fbc547a4715a7461b3eac5436",
+    "url": "/flower_website/static/media/7.28316b0f.jpg"
+  },
+  {
+    "revision": "bbaec4290460e4b37f06aa2c3d0c3837",
+    "url": "/flower_website/static/media/8.bbaec429.jpg"
+  },
+  {
+    "revision": "37564f040c2114155b842dfcb06f1538",
+    "url": "/flower_website/static/media/9.37564f04.jpg"
+  },
+  {
+    "revision": "8801cd8b2132376f4192aec591acc830",
+    "url": "/flower_website/index.html"
+  }
+];
